46b532c5570cf170cdc608fc8adc56f67bd85498
samantha <samsheehan08@gmail.com>
2016-10-11 09:58:08 +0100

    update

---
+++ src/controllers/PacemakerAPI.java
@@ -0,0 +1,82 @@
+package controllers;
+
+import java.util.Collection;
+import com.google.common.base.Optional;
+import java.util.HashMap;
+import java.util.Map;
+import models.Activity;
+import models.Location;
+import models.User;
+
+public class PacemakerAPI
+{
+  private Map<Long,   User>   userIndex       = new HashMap<>();
+  private Map<String, User>   emailIndex      = new HashMap<>();
+  private Map<Long, Activity> activitiesIndex = new HashMap<>();
+      
+  public PacemakerAPI()
+  {
+  }
+  
+  public Collection<User> getUsers ()
+  {
+    return userIndex.values();
+  }
+  
+  public  void deleteUsers() 
+  {
+    userIndex.clear();
+    emailIndex.clear();
+  }
+  
+  public User createUser(String firstName, String lastName, String email, String password) 
+  {
+    User user = new User (firstName, lastName, email, password);
+    userIndex.put(user.id, user);
+    emailIndex.put(email, user);
+    return user;
+  }
+  
+  public User getUserByEmail(String email) 
+  {
+    return emailIndex.get(email);
+  }
+
+  public User getUser(Long id) 
+  {
+    return userIndex.get(id);
+  }
+
+  public void deleteUser(Long id) 
+  {
+    User user = userIndex.remove(id);
+    emailIndex.remove(user.email);
+  }
+  
+  public Activity createActivity(Long id, String type, String location, double distance)
+  {
+    Activity activity = null;
+    Optional<User> user = Optional.fromNullable(userIndex.get(id));
+    if (user.isPresent())
+    {
+      activity = new Activity (type, location, distance);
+      user.get().activities.put(activity.id, activity);
+      activitiesIndex.put(activity.id, activity);
+    }
+    return activity;
+  }
+  
+  public Activity getActivity (Long id)
+  {
+    return activitiesIndex.get(id);
+  }
+  
+  public void addLocation (Long id, float latitude, float longitude)
+  {
+    Optional<Activity> activity = Optional.fromNullable(activitiesIndex.get(id));
+    if (activity.isPresent())
+    {
+      activity.get().route.add(new Location(latitude, longitude));
+    }
+  }
+}
---
+++ src/models/Activity.java
@@ -0,0 +1,67 @@
+package models;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.base.Objects;
+
+public class Activity 
+{ 
+  static Long   counter = 0l;
+  
+  public Long   id;
+  
+  public String type;
+  public String location;
+  public double distance;
+  
+  public List<Location> route = new ArrayList<>();
+ 
+  public Activity()
+  {
+  }
+  
+  public Activity(String type, String location, double distance)
+  {
+    this.id        = counter++;
+    this.type      = type;
+    this.location  = location;
+    this.distance  = distance;
+  }
+  
+  @Override
+  public String toString()
+  {
+    return toStringHelper(this).addValue(id)
+                               .addValue(type)
+                               .addValue(location)
+                               .addValue(distance)
+                               .addValue(route)
+                               .toString();
+  }
+  
+  @Override  
+  public int hashCode()  
+  {  
+     return Objects.hashCode(this.id, this.type, this.location, this.distance);  
+  } 
+  
+  @Override
+  public boolean equals(final Object obj)
+  {
+    if (obj instanceof Activity)
+    {
+      final Activity other = (Activity) obj;
+      return Objects.equal(type, other.type) 
+          && Objects.equal(location,  other.location)
+          && Objects.equal(distance,  other.distance)
+          && Objects.equal(route,     other.route);    
+    }
+    else
+    {
+      return false;
+    }
+  }
+}
